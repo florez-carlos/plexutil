e27d2b454b88be7aab23bfeaeb4e2b8e103a8f0a
Carlos Florez <carlos@florez.co.uk>
2024-05-30 18:10:08 -0700

    feat(Exceptions): Add new PlexUtilConfig exception

---
+++ src/exception/PlexUtilConfigException.py
@@ -0,0 +1,13 @@
+class PlexUtilConfigException(Exception):
+    def __init__(self, original_exception=None):
+
+        self.original_exception = original_exception
+
+        if self.original_exception:
+            message = "INVALID PLEX UTIL CONFIG | %s" % (self.original_exception.args[0])
+            self.__cause__ = original_exception
+        else:
+            message = "INVALID PLEX UTIL CONFIG"
+
+        super().__init__(message)
+
