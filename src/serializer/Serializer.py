c68f8ecc97aafd0d5992e858b6bf3270811261cb
Carlos Florez <carlos@florez.co.uk>
2024-06-27 17:41:56 -0700

    feat(Serializable): Add Serializer and Serializable

---
+++ src/serializer/Serializer.py
@@ -0,0 +1,14 @@
+from abc import ABC, abstractmethod
+
+from src.serializer.Serializable import Serializable
+
+class Serializer(ABC):
+
+    @abstractmethod
+    def to_json(self, serializable) -> dict:
+        pass
+
+    @abstractmethod
+    def to_dto(self, json_dict: dict) -> Serializable:
+        pass
+    
