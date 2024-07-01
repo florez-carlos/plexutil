98a9b8bc7568fce165b39173717fa2ceb2c0ff84
Carlos Florez <carlos@florez.co.uk>
2024-07-01 13:38:09 -0700

    feat(MusicPlaylistFileDTO)!: Add Serializable, remove location property

--- src/dto/MusicPlaylistFileDTO.py
+++ src/dto/MusicPlaylistFileDTO.py
@@ -1,12 +1,12 @@
 from dataclasses import dataclass, field
 from pathlib import Path
 from typing import List
+from src.serializer.Serializable import Serializable
 from src.dto.MusicPlaylistDTO import MusicPlaylistDTO
 
 @dataclass(frozen=True)
-class MusicPlaylistFileDTO():
+class MusicPlaylistFileDTO(Serializable):
 
-    location: Path = Path()
     track_count: int = 0
     playlists: List[MusicPlaylistDTO] = field(default_factory=lambda: [])
 
@@ -16,7 +16,7 @@ def __eq__(self, other):
     if not isinstance(other, MusicPlaylistFileDTO):
         return NotImplemented
 
-    return self.location == other.location and self.track_count == other.track_count and self.playlists == other.playlists
+    return self.track_count == other.track_count and self.playlists == other.playlists
 
 def __hash__(self):
     return hash((self.location, self.track_count,self.playlists))
