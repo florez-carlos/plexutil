fcbab3189b3a63b885cf8c6b9a0b18cb17c60085
Carlos Florez <carlos@florez.co.uk>
2024-07-04 11:25:18 -0700

    chore(MovieLibrary): Remove query implementation, replace prefs fie...
    location for LibraryPreferencesDTO

--- src/core/MovieLibrary.py
+++ src/core/MovieLibrary.py
@@ -1,36 +1,28 @@
 from pathlib import Path
 from typing import List
-from alive_progress import alive_bar
 from plexapi.audio import Audio
 from plexapi.server import PlexServer
-from plexapi.utils import json
 from throws import throws
-from src.exception.ExpectedLibraryCountException import ExpectedLibraryCountException
+from src.dto.LibraryPreferencesDTO import LibraryPreferencesDTO
 from src.exception.LibraryOpException import LibraryOpException
 from src.enum.Agent import Agent
 from src.enum.Language import Language
 from src.enum.LibraryName import LibraryName
 from src.enum.LibraryType import LibraryType
 from src.enum.Scanner import Scanner
-from src.dto.MusicPlaylistFileDTO import MusicPlaylistFileDTO
 from src.core.Library import Library
-from src.util.QueryBuilder import QueryBuilder
 
-import time
-
-from src.util.PlexOps import PlexOps
 
 
 class MovieLibrary(Library):
 
     def __init__(self,
                  plex_server: PlexServer,
-                 name: LibraryName,
                  location:Path,
                  language: Language,
-                 prefs_file_location: Path):
+                 preferences: LibraryPreferencesDTO):
         
-        super().__init__(plex_server,name,LibraryType.MOVIE,Agent.MOVIE,Scanner.MOVIE,location,language,prefs_file_location)
+        super().__init__(plex_server,LibraryName.MOVIE,LibraryType.MOVIE,Agent.MOVIE,Scanner.MOVIE,location,language,preferences)
         
 
 
@@ -51,15 +43,8 @@ class MovieLibrary(Library):
             #This line triggers a refresh of the library
             self.plex_server.library.sections()
 
-            prefs = {}
 
-            with self.prefs_file_location.open(encoding='utf-8') as file:
-                
-                file_dict = json.load(file)
-                prefs = file_dict.get("prefs")
-
-
-            self.plex_server.library.section(self.name).editAdvanced(**prefs)
+            self.plex_server.library.section(self.name).editAdvanced(**self.preferences.movie)
 
         except LibraryOpException as e:
             raise e
@@ -86,15 +71,4 @@ class MovieLibrary(Library):
             raise e
         except Exception as e:
             raise LibraryOpException("DELETE", original_exception=e)
-
-    @throws(LibraryOpException)
-    def query(self) -> List[Audio]:
-
-        try:
-            
-            return self.plex_server.library.section(self.name).searchTracks()
-            
-        except Exception as e:
-            
-                raise LibraryOpException("QUERY", original_exception=e)
 
