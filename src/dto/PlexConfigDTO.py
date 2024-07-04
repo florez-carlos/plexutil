11344a717b0598645df50991659576af62ff86ee
Carlos Florez <carlos@florez.co.uk>
2024-07-04 11:20:44 -0700

    chore(PlexConfigDTO): Remove music playlist file location

--- src/dto/PlexConfigDTO.py
+++ src/dto/PlexConfigDTO.py
@@ -10,7 +10,7 @@ class PlexConfigDTO(Serializable):
     music_folder_path: Path = Path()
     movie_folder_path: Path = Path()
     tv_folder_path: Path = Path()
-    music_playlist_file_path: Path = Path()
+    # music_playlist_file_path: Path = Path()
     host: str = "localhost"
     port: int = 32000
     token: str = ""
@@ -23,10 +23,9 @@ def __eq__(self, other):
     return self.music_folder_path == other.music_folder_path\
     and self.movie_folder_path == other.movie_folder_path\
     and self.tv_folder_path == other.tv_folder_path\
-    and self.music_playlist_file_path == other.music_playlist_file_path\
     and self.host == other.host\
     and self.port == other.port\
     and self.token == other.token
 
 def __hash__(self):
-    return hash((self.music_folder_path, self.movie_folder_path, self.tv_folder_path,self.music_playlist_file_path,self.host,self.port,self.token))
+    return hash((self.music_folder_path, self.movie_folder_path, self.tv_folder_path,self.host,self.port,self.token))
