2e7e7a5555f3a3d38ad116f9405f1e2fdb372a23
Carlos Florez <carlos@florez.co.uk>
2024-07-04 11:28:44 -0700

    feat(Library): Add poll and query implementations...
    Add LibraryPreferencesDTO member var

--- src/core/Library.py
+++ src/core/Library.py
@@ -1,9 +1,13 @@
+import time
 from pathlib import Path
-from typing import List, overload
+from typing import Dict, List, overload
+from alive_progress import alive_bar
 from plexapi.audio import Audio
 from plexapi.server import PlexServer
 from plexapi.video import Video
 from throws import throws
+from src.dto.LibraryPreferencesDTO import LibraryPreferencesDTO
+from src.exception.ExpectedLibraryCountException import ExpectedLibraryCountException
 from src.exception.LibraryOpException import LibraryOpException
 from src.dto.MusicPlaylistDTO import MusicPlaylistDTO
 from src.enum.Agent import Agent
@@ -25,9 +29,7 @@ class Library(ABC):
                  scanner: Scanner,
                  location: Path,
                  language: Language,
-                 prefs_file_location: Path):
-                 # tv_library_language_manifest_file_location: Path,
-                 # music_playlist_file_dto: MusicPlaylistFileDTO) -> None:
+                 preferences: LibraryPreferencesDTO):
         self.plex_server = plex_server
         self.name = name
         self.library_type = library_type
@@ -35,8 +37,7 @@ class Library(ABC):
         self.scanner = scanner
         self.location = location
         self.language = language
-        self.prefs_file_location = prefs_file_location
-        # self.music_playlist_file_dto = music_playlist_file_dto
+        self.preferences = preferences
 
     @abstractmethod
     @throws(LibraryOpException)
@@ -48,7 +49,72 @@ class Library(ABC):
     def delete(self) -> None:
         pass
 
-    @abstractmethod
-    def query(self) -> List[Audio] | List[Video]:
-        pass
+
+    def poll(self, requested_attempts: int = 0, expected_count: int = 0,interval_seconds: int = 0, tvdb_ids: List[int] = []) -> None:
+
+
+        current_count = len(self.query())
+
+        offset = abs(expected_count - current_count)
+        print("Requested attempts: "+str(requested_attempts))
+        print("Interval seconds: "+str(interval_seconds))
+        print("Current count: "+str(current_count)+". Expected: "+str(expected_count))
+        print("Expected net change: "+str(offset))
+
+        with alive_bar(offset) as bar:
+            
+            attempts = -1
+
+            while(attempts<requested_attempts):
+
+                time.sleep(interval_seconds)
+                attempts = attempts+1
+                if attempts >= requested_attempts:
+                    raise ExpectedLibraryCountException("TIMEOUT: Did not reach expected count")
+
+                if tvdb_ids:
+                    updated_current_count = len(self.query(tvdb_ids))
+                else:
+                    updated_current_count = len(self.query())
+
+                offset = abs(updated_current_count - current_count)
+
+                if offset == 0:
+                    continue
+                else:
+                    current_count = updated_current_count
+                    for j in range(offset):
+                        bar()
+
+        return
+
+
+    def query(self,tvdb_ids: List[int] = []) -> List[Audio] | List[Video]:
+
+        if (self.library_type is LibraryType.MUSIC):
+            if tvdb_ids:
+                raise ValueError("Library type: "+ 
+                    LibraryType.MUSIC.value +
+                    " not compatible with tvdb ids but tvdb ids supplied: " +
+                    str(tvdb_ids))
+            return self.plex_server.library.section(self.name).searchTracks()
+        elif (self.library_type is LibraryType.TV):
+
+            shows = self.plex_server.library.section(self.name).all()
+            shows_filtered = []
+
+            if tvdb_ids:
+
+                for show in shows:
+                    guids = show.guids
+                    for guid in guids:
+                        if "tvdb://" in guid.id:
+                            tvdb = guid.id.replace("tvdb://","")
+                            if int(tvdb) in tvdb_ids:
+                                shows_filtered.append(show)
+
+            return shows_filtered
+
+        else:
+            raise ExpectedLibraryCountException("Unsupported Library Type: " + self.library_type.value)
 
