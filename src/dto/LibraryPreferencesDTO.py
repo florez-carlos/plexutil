d8768f1777471d9e7f890d4068f3a8920e9b22f5
Carlos Florez <carlos@florez.co.uk>
2024-07-09 17:25:23 -0700

    feat(LibraryPreferencesDTO): Add plex_server_setting property

--- src/dto/LibraryPreferencesDTO.py
+++ src/dto/LibraryPreferencesDTO.py
@@ -8,13 +8,14 @@ class LibraryPreferencesDTO():
     music: Dict
     movie: Dict
     tv: Dict
+    plex_server_settings: Dict
 
 def __eq__(self, other):
 
     if not isinstance(other, LibraryPreferencesDTO):
         return NotImplemented
 
-    return self.music == other.music and self.movie == other.movie and self.tv == other.tv 
+    return self.music == other.music and self.movie == other.movie and self.tv == other.tv and self.plex_server_settings == other.plex_server_settings
     
 def __hash__(self):
-    return hash((self.prefs))
+    return hash((self.music, self.movie, self.tv, self.plex_server_settings))
