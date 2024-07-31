356b76214c4f195e5192d816beb67d87efbd381e
Carlos Florez <carlos@florez.co.uk>
2024-07-31 14:31:33 -0700

    refactor(main): rework exception handling and server init location

--- src/plexutil/__main__.py
+++ src/plexutil/__main__.py
@@ -1,7 +1,4 @@
-import ctypes.wintypes
-import os
-import platform
-from pathlib import Path
+import sys
 
 from plexapi.server import PlexServer
 
@@ -13,97 +10,43 @@ from plexutil.core.tv_library import TVLibrary
 from plexutil.dto.music_playlist_file_dto import MusicPlaylistFileDTO
 from plexutil.enums.language import Language
 from plexutil.enums.user_request import UserRequest
+from plexutil.exception.bootstrap_error import BootstrapError
+from plexutil.exception.plex_util_config_error import PlexUtilConfigError
 from plexutil.plex_util_logger import PlexUtilLogger
 from plexutil.util.file_importer import FileImporter
-from plexutil.util.path_ops import PathOps
 from plexutil.util.plex_ops import PlexOps
-
-logger = None
 
 
 def main() -> None:
-    home_folder = Path()
-    plex_util_dir = Path()
-    config_dir = Path()
-    log_dir = Path()
-    system = platform.system()
-    if system == "Windows":
-        csidl_personal = 5
-        shgfp_type_current = 0
-
-        buf = ctypes.create_unicode_buffer(ctypes.wintypes.MAX_PATH)
-        ctypes.windll.shell32.SHGetFolderPathW(  # pyright: ignore # noqa: PGH003
-            None, csidl_personal, None, shgfp_type_current, buf
-        )
-        home_folder = buf.value or ""
-        if not home_folder:
-            description = "Could not locate Documents folder"
-            raise FileNotFoundError(description)
-    elif system == "Linux":
-        home_folder = os.getenv("HOME") or ""
-    else:
-        description = f"Unsupported OS: {system}"
-        raise OSError(description)
-
-    plex_util_dir = Path(home_folder) / "plexutil"
-    config_dir = plex_util_dir / "config"
-    log_dir = plex_util_dir / "log"
-
-    plex_util_dir.mkdir(exist_ok=True)
-    config_dir.mkdir(exist_ok=True)
-    log_dir.mkdir(exist_ok=True)
-
-    log_config_file_path = (
-        PathOps.get_project_root() / "plexutil" / "config" / "log_config.yaml"
-    )
-
-    PlexUtilLogger(log_dir, log_config_file_path)
-
     try:
-        config_file_path = config_dir / "config.json"
-        instructions_dto = Prompt.get_user_instructions_dto(config_file_path)
+        bootstrap_paths_dto = FileImporter.bootstrap()
+
+        config_dir = bootstrap_paths_dto.config_dir
+        plexutil_config_file = bootstrap_paths_dto.plexutil_config_file
+
+        instructions_dto = Prompt.get_user_instructions_dto(
+            plexutil_config_file
+        )
 
         request = instructions_dto.request
         items = instructions_dto.items
         plex_config_dto = instructions_dto.plex_config_dto
 
-        host = plex_config_dto.host
-        port = int(plex_config_dto.port)
-        token = plex_config_dto.token
-
-        baseurl = f"http://{host}:{port}"
-        plex_server = PlexServer(baseurl, token)
+        if request == UserRequest.CONFIG:
+            sys.exit(0)
 
         music_location = instructions_dto.plex_config_dto.music_folder_path
         movie_location = instructions_dto.plex_config_dto.movie_folder_path
         tv_location = instructions_dto.plex_config_dto.tv_folder_path
 
-        music_prefs_file_location = (
-            config_dir / "music_library_preferences.json"
-        )
-        movie_prefs_file_location = (
-            config_dir / "movie_library_preferences.json"
-        )
-        tv_prefs_file_location = config_dir / "tv_library_preferences.json"
-        plex_server_setting_prefs_file_location = (
-            config_dir / "plex_server_setting_preferences.json"
-        )
-        music_playlist_file_location = config_dir / "music_playlists.json"
-        tv_language_manifest_file_location = (
-            config_dir / "tv_language_manifest.json"
-        )
-
         preferences_dto = FileImporter.get_library_preferences_dto(
-            music_prefs_file_location,
-            movie_prefs_file_location,
-            tv_prefs_file_location,
-            plex_server_setting_prefs_file_location,
+            config_dir,
         )
         music_playlist_file_dto = FileImporter.get_music_playlist_file_dto(
-            music_playlist_file_location,
+            config_dir,
         )
         tv_language_manifest_file_dto = FileImporter.get_tv_language_manifest(
-            tv_language_manifest_file_location,
+            config_dir,
         )
 
         playlists = []
@@ -123,6 +66,13 @@ def main() -> None:
 
         if instructions_dto.is_all_items:
             music_playlist_file_dto_filtered = music_playlist_file_dto
+
+        host = plex_config_dto.host
+        port = int(plex_config_dto.port)
+        token = plex_config_dto.token
+
+        baseurl = f"http://{host}:{port}"
+        plex_server = PlexServer(baseurl, token)
 
         match request:
             # If config, we should already be done by now
@@ -261,10 +211,19 @@ def main() -> None:
             PlexUtilLogger.get_logger().exception(description)
             raise
 
-    except:
+    except PlexUtilConfigError:
+        description = "Plexutil configuration error"
+        PlexUtilLogger.get_logger().exception(description)
+
+    # No regular logger can be expected to be initialized
+    except BootstrapError as e:
+        description = "\n\n=====Program initialization error=====\n\n" f"{e!s}"
+        e.args = (description,)
+        raise
+
+    except Exception:  # noqa: BLE001
         description = "Unexpected error:"
         PlexUtilLogger.get_logger().exception(description)
-        raise
 
 
 if __name__ == "__main__":
