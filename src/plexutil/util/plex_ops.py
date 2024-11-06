54793dafbe3d520527b01acce75e0687ae2693cd
Carlos Florez <carlos@florez.co.uk>
2024-11-06 13:21:45 -0800

    feat(plexops): Add method to fetch local songs

--- src/plexutil/util/plex_ops.py
+++ src/plexutil/util/plex_ops.py
@@ -1,7 +1,16 @@
-from plexapi.server import PlexServer
+from __future__ import annotations
 
-from plexutil.dto.library_preferences_dto import LibraryPreferencesDTO
+from pathlib import Path
+from typing import TYPE_CHECKING
+
+from plexutil.dto.song_dto import SongDTO
+from plexutil.enums.file_type import FileType
 from plexutil.static import Static
+
+if TYPE_CHECKING:
+    from plexapi.server import PlexServer
+
+    from plexutil.dto.library_preferences_dto import LibraryPreferencesDTO
 
 
 class PlexOps(Static):
@@ -14,3 +23,22 @@ class PlexOps(Static):
         for setting_id, setting_value in server_settings.items():
             plex_server.settings.get(setting_id).set(setting_value)
         plex_server.settings.save()
+
+    @staticmethod
+    def get_local_songs(music_folder_path: Path) -> list[SongDTO]:
+        songs = []
+
+        for item in Path(music_folder_path).iterdir():
+            if item.is_file():
+                file_name = item.stem
+                file_extension = item.suffix
+                songs.append(
+                    SongDTO(
+                        name=file_name,
+                        extension=FileType.get_file_type_from_str(
+                            file_extension
+                        ),
+                    )
+                )
+
+        return songs
