e0418cced445d5a637c2c2678905c0be38b96e34
Carlos Florez <carlos@florez.co.uk>
2024-11-24 16:09:53 -0800

    feat(UserInstructionsDTO): Add library_name

--- src/plexutil/dto/user_instructions_dto.py
+++ src/plexutil/dto/user_instructions_dto.py
@@ -16,6 +16,7 @@ if TYPE_CHECKING:
 class UserInstructionsDTO:
     request: UserRequest
     library_type: LibraryType
+    library_name: str
     server_config_dto: ServerConfigDTO
     language: Language = Language.ENGLISH_US
     locations: list[Path] = field(default_factory=list)
@@ -29,6 +30,7 @@ class UserInstructionsDTO:
         return (
             self.request == other.request
             and self.library_type == other.library_type
+            and self.library_name == other.library_name
             and self.items == other.items
             and self.locations == other.locations
             and self.is_all_items == other.is_all_items
@@ -40,6 +42,7 @@ class UserInstructionsDTO:
             (
                 self.request,
                 self.library_type,
+                self.library_name,
                 self.items,
                 self.locations,
                 self.is_all_items,
