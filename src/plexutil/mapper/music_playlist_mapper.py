56d2935ff1ca9131bee7e2e946295551083cabe6
Carlos Florez <carlos@florez.co.uk>
2024-11-27 15:10:48 -0800

    feat(MusicPlaylistMapper): Add get_entity method

--- src/plexutil/mapper/music_playlist_mapper.py
+++ src/plexutil/mapper/music_playlist_mapper.py
@@ -5,3 +5,6 @@ from plexutil.model.music_playlist_entity import MusicPlaylistEntity
 class MusicPlaylistMapper:
     def get_dto(self, entity: MusicPlaylistEntity) -> MusicPlaylistDTO:
         return MusicPlaylistDTO(name=str(entity.name), songs=[])
+
+    def get_entity(self, dto: MusicPlaylistDTO) -> MusicPlaylistEntity:
+        return MusicPlaylistEntity(name=str(dto.name))
