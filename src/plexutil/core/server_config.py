3f56f2fbb009f743f8261ca13a02aa7d3a446256
Carlos Florez <carlos@florez.co.uk>
2024-12-03 17:06:11 -0800

    refactor(ServerConfig): Add TokenManager usage

--- src/plexutil/core/server_config.py
+++ src/plexutil/core/server_config.py
@@ -1,12 +1,10 @@
-from peewee import DoesNotExist, SqliteDatabase
-
 from plexutil.dto.bootstrap_paths_dto import BootstrapPathsDTO
 from plexutil.dto.server_config_dto import ServerConfigDTO
 from plexutil.exception.server_config_error import ServerConfigError
 from plexutil.mapper.server_config_mapper import ServerConfigMapper
-from plexutil.model.server_config_entity import ServerConfigEntity
 from plexutil.plex_util_logger import PlexUtilLogger
 from plexutil.service.server_config_service import ServerConfigService
+from plexutil.util.token_manager import TokenManager
 
 
 class ServerConfig:
@@ -15,37 +13,54 @@ class ServerConfig:
         bootstrap_paths_dto: BootstrapPathsDTO,
         server_config_dto: ServerConfigDTO,
     ):
-        self.bootstrap_paths_dto = bootstrap_paths_dto
         self.server_config_dto = server_config_dto
+        self.service = ServerConfigService(
+            bootstrap_paths_dto.config_dir / "config.db"
+        )
+        self.mapper = ServerConfigMapper()
 
-    def setup(self) -> ServerConfigDTO:
-        with SqliteDatabase(
-            self.bootstrap_paths_dto.config_dir / "plexutil.db"
-        ) as db:
-            db.bind([ServerConfigEntity])
+    def get(self) -> ServerConfigDTO:
+        server_config_dto = self.mapper.get_dto(self.service.get())
+        token = TokenManager.decrypt(server_config_dto.token)
 
-            db.create_tables(
-                [ServerConfigEntity],
-                safe=True,
+        dto = ServerConfigDTO(
+            host=server_config_dto.host,
+            port=server_config_dto.port,
+            token=token,
+        )
+
+        return dto
+
+    def save(self) -> ServerConfigDTO:
+        if not self.server_config_dto.token:
+            description = (
+                "Token has not been supplied. Example: "
+                "plexutil config -host ... -port ... -token ..."
             )
-            try:
-                return ServerConfigMapper.get_dto(ServerConfigService.get())
-            except DoesNotExist:
-                if not self.server_config_dto.token:
-                    description = "No Plex Token has been supplied"
-                    PlexUtilLogger.get_logger().error(description)
-                    raise ServerConfigError(description)
+            raise ServerConfigError(description)
 
-                server_config_entity = ServerConfigMapper.get_entity(
-                    self.server_config_dto,
-                )
+        encrypted_token = TokenManager.encrypt(self.server_config_dto.token)
+        dto = ServerConfigDTO(
+            host=self.server_config_dto.host,
+            port=self.server_config_dto.port,
+            token=encrypted_token,
+        )
+        self.service.save(self.mapper.get_entity(dto))
+        entity = self.service.get()
+        server_config_dto = self.mapper.get_dto(entity)
+        token = TokenManager.decrypt(server_config_dto.token)
 
-                description = (
-                    "Plexutil Config does not exist. Creating:\n"
-                    f"Host: {server_config_entity.host}"
-                    f"Port: {server_config_entity.port}"
-                    f"Token: {server_config_entity.token}"
-                )
-                PlexUtilLogger.get_logger().debug(description)
-                ServerConfigService.add(server_config_entity)
-                return ServerConfigMapper.get_dto(ServerConfigService.get())
+        dto = ServerConfigDTO(
+            host=server_config_dto.host,
+            port=server_config_dto.port,
+            token=token,
+        )
+        description = (
+            f"Created a server config:\n"
+            f"Host: {server_config_dto.host}\n"
+            f"Port: {server_config_dto.port}\n"
+            f"Token supplied: {'YES' if server_config_dto.token else 'NO'}\n"
+        )
+        PlexUtilLogger.get_logger().debug(description)
+
+        return dto
