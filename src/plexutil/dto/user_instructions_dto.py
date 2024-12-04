275a92210e9c1e6fcd42312f7cd0a9c6039e06ed
Carlos Florez <carlos@florez.co.uk>
2024-12-03 17:01:03 -0800

    refactor(UserInstructions): Replace some variables

--- src/plexutil/dto/user_instructions_dto.py
+++ src/plexutil/dto/user_instructions_dto.py
@@ -17,12 +17,13 @@ class UserInstructionsDTO:
     request: UserRequest
     library_type: LibraryType
     library_name: str
+    playlist_name: str
     server_config_dto: ServerConfigDTO = ServerConfigDTO()
     is_show_configuration: bool = False
+    is_show_configuration_token: bool = False
     language: Language = Language.ENGLISH_US
     locations: list[Path] = field(default_factory=list)
-    items: list[str] = field(default_factory=list)
-    is_all_items: bool = False
+    songs: list[str] = field(default_factory=list)
 
     def __eq__(self, other: object) -> bool:
         if not isinstance(other, UserInstructionsDTO):
@@ -32,11 +33,13 @@ class UserInstructionsDTO:
             self.request == other.request
             and self.library_type == other.library_type
             and self.library_name == other.library_name
+            and self.playlist_name == other.playlist_name
             and self.is_show_configuration == other.is_show_configuration
+            and self.is_show_configuration_token
+            == other.is_show_configuration_token
             and self.language is other.language
             and self.locations == other.locations
-            and self.items == other.items
-            and self.is_all_items is other.is_all_items
+            and self.songs == other.songs
             and self.server_config_dto == other.server_config_dto
         )
 
@@ -46,11 +49,12 @@ class UserInstructionsDTO:
                 self.request,
                 self.library_type,
                 self.library_name,
+                self.playlist_name,
                 self.server_config_dto,
                 self.is_show_configuration,
+                self.is_show_configuration_token,
                 self.language,
                 self.locations,
-                self.items,
-                self.is_all_items,
+                self.songs,
             ),
         )
