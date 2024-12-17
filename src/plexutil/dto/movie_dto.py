14340b31a5a418218d6334b8d9b118900e45dd21
Carlos Florez <carlos@florez.co.uk>
2024-12-17 15:26:46 -0800

    feat(DTO): Add new DTOs

---
+++ src/plexutil/dto/movie_dto.py
@@ -0,0 +1,23 @@
+from dataclasses import dataclass
+
+from plexutil.enums.file_type import FileType
+
+
+@dataclass(frozen=True)
+class MovieDTO:
+    name: str = ""
+    extension: FileType = FileType.UNKNOWN
+    year: int = 0
+
+    def __eq__(self, other: object) -> bool:
+        if not isinstance(other, MovieDTO):
+            return False
+
+        return (
+            self.name == other.name
+            and self.extension == other.extension
+            and self.year == other.year
+        )
+
+    def __hash__(self) -> int:
+        return hash((self.name, self.extension, self.year))
---
+++ src/plexutil/dto/tv_episode_dto.py
@@ -0,0 +1,25 @@
+from dataclasses import dataclass
+
+
+@dataclass(frozen=True)
+class TVEpisodeDTO:
+    name: str = ""
+    first_aired_year: int = 0
+    season: int = 0
+    episode: int = 0
+
+    def __eq__(self, other: object) -> bool:
+        if not isinstance(other, TVEpisodeDTO):
+            return False
+
+        return (
+            self.name == other.name
+            and self.first_aired_year == other.first_aired_year
+            and self.season == other.season
+            and self.episode == other.episode
+        )
+
+    def __hash__(self) -> int:
+        return hash(
+            (self.name, self.first_aired_year, self.season, self.episode)
+        )
