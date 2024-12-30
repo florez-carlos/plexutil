9a62b22af72a0269eb8dd9a4cde8d5fb818b2911
Carlos Florez <carlos@florez.co.uk>
2024-12-30 14:51:21 -0800

    refactor(ServerConfigDTO): Replace defaults with None

--- src/plexutil/dto/server_config_dto.py
+++ src/plexutil/dto/server_config_dto.py
@@ -1,27 +1,9 @@
 from dataclasses import dataclass
 
 
+# Frozen=True creates an implicit hash method, eq is created by default
 @dataclass(frozen=True)
 class ServerConfigDTO:
-    host: str = ""
-    port: int = 0
-    token: str = ""
-
-    def __eq__(self, other: object) -> bool:
-        if not isinstance(other, ServerConfigDTO):
-            return False
-
-        return (
-            self.host == other.host
-            and self.port == other.port
-            and self.token == other.token
-        )
-
-    def __hash__(self) -> int:
-        return hash(
-            (
-                self.host,
-                self.port,
-                self.token,
-            ),
-        )
+    host: str | None = None
+    port: int | None = None
+    token: str | None = None
