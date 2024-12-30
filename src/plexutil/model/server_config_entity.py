80849295f2d2702cf7a96e7c7df587211ef6023c
Carlos Florez <carlos@florez.co.uk>
2024-12-30 14:52:31 -0800

    refactor(ServerConfigEntity): Allow nullable fields, set defaults None

--- src/plexutil/model/server_config_entity.py
+++ src/plexutil/model/server_config_entity.py
@@ -3,9 +3,9 @@ from peewee import IntegerField, Model, TextField
 
 class ServerConfigEntity(Model):
     id = IntegerField(primary_key=True, default=1)
-    host = TextField(null=False)
-    port = IntegerField(null=False)
-    token = TextField(null=False)
+    host = TextField(null=True, default=None)
+    port = IntegerField(null=True, default=None)
+    token = TextField(null=True, default=None)
 
     class Meta:
         table_name = "server_config"
