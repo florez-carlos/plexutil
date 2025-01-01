34aa861a08bbaf49bcf714847a796143ca380ae3
Carlos Florez <carlos@florez.co.uk>
2024-12-31 16:51:58 -0800

    Revert "refactor(MusicLibrary): Replace list of Locations with dict"
    This reverts commit ac7f3494da7542f3dba834891b167b829cdfee0f.

--- src/plexutil/core/music_library.py
+++ src/plexutil/core/music_library.py
@@ -67,11 +67,6 @@ class MusicLibrary(Library):
         self.log_library(operation=op_type, is_info=False, is_debug=True)
 
         part = ""
-        locations = {}
-        count = 1
-        for location in self.locations:
-            locations[f"location{count}"] = location
-            count = count + 1
 
         query_builder = QueryBuilder(
             "/library/sections",
@@ -82,7 +77,7 @@ class MusicLibrary(Library):
             language=self.language.value,
             importFromiTunes="",
             enableAutoPhotoTags="",
-            location=locations,
+            location=self.locations,
             prefs=self.preferences.music,
         )
 
