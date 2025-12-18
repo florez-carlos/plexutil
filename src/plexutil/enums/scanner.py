8bb38f6fff774ac6c2ca9c72f0ed24c1bcf372ca
Carlos Florez <carlos@florez.co.uk>
2025-12-18 00:07:18 +0000

    refactor(Agent/Scanner): Add support for all Agents and Scanners

--- src/plexutil/enums/scanner.py
+++ src/plexutil/enums/scanner.py
@@ -2,38 +2,145 @@ from __future__ import annotations
 
 from enum import Enum
 
+from plexutil.enums.library_type import LibraryType
+from plexutil.exception.library_unsupported_error import (
+    LibraryUnsupportedError,
+)
+from plexutil.exception.user_error import UserError
 from src.plexutil.plex_util_logger import PlexUtilLogger
 
 
 # Evaluates file name/folder structure to identify media
 # https://support.plex.tv/articles/200241548-scanners/
 class Scanner(Enum):
-    MUSIC = "Plex Music"
-    TV = "Plex TV Series"
     MOVIE = "Plex Movie"
-    MOVIE_VIDEO = "Plex Video Files Scanner"  # Most appropriate for Home Video
+    MOVIE_VIDEO = "Plex Video Files Scanner"
     MOVIE_LEGACY = "Plex Movie Scanner"
+    TV = "Plex TV Series"
     TV_LEGACY = "Plex Series Scanner"
+    MUSIC = "Plex Music"
+    MUSIC_LEGACY = "Plex Music Scanner"
 
     @staticmethod
     def get_all() -> list[Scanner]:
         return list(Scanner)
 
+    def is_deprecated(self) -> bool:
+        """
+        Based on: https://support.plex.tv/articles/200241548-scanners/
+
+        Returns:
+            bool: Is this a Legacy Scanner
+        """
+        deprecated = [
+            Scanner.MOVIE_LEGACY,
+            Scanner.TV_LEGACY,
+            Scanner.MUSIC_LEGACY,
+        ]
+        return self in deprecated
+
+    def is_compatible(self, library_type: LibraryType) -> bool:
+        """
+        Ensures this Scanner is compatible with the supplied LibraryType
+
+        Args:
+            library_type (LibraryType): Check compatibility with this Scanner
+
+        Returns:
+            bool: Is this Scanner compatible with the supplied LibraryType
+        Raises:
+            LibraryUnsupportedError: If supplied Library not Movie/TV/Music
+        """
+        if library_type is LibraryType.MOVIE:
+            return (
+                self is Scanner.MOVIE
+                or self is Scanner.MOVIE_VIDEO
+                or self is Scanner.MOVIE_LEGACY
+            )
+        elif library_type is LibraryType.TV:
+            return self is Scanner.TV or self is Scanner.TV_LEGACY
+        elif library_type is LibraryType.MUSIC:
+            return self is Scanner.MUSIC or self is Scanner.MUSIC_LEGACY
+        else:
+            op_type = "Scanner Is Compatible"
+            raise LibraryUnsupportedError(op_type, library_type)
+
     @staticmethod
-    def get_from_str(candidate: str) -> Scanner:
+    def get_from_str(candidate: str, library_type: LibraryType) -> Scanner:
+        """
+        Get a Scanner from its str representation
+        Logs a Warning if the Scanner is deprecated
+
+        Args:
+            candidate (str): The likely Scanner
+            library_type (LibraryType): To check compatibility with the Scanner
+
+        Returns:
+            Scanner: Matched from the candidate str
+
+        Raises:
+            UserError: If Scanner incompatible with the supplied LibraryType
+                       If Scanner couldn't be determined from the candidate str
+        """
         for scanner in Scanner.get_all():
-            if candidate.lower() == scanner.value.lower():
-                if (
-                    scanner is Scanner.MOVIE_LEGACY
-                    or scanner is Scanner.TV_LEGACY
-                ):
+            if candidate.lower() == scanner.get_label().lower():
+                if not scanner.is_compatible(library_type):
                     description = (
-                        f"WARNING: Chosen a deprecated Scanner "
-                        f"({scanner.value})"
+                        f"Chosen Scanner ({scanner.get_label()}) "
+                        f"is not compatible with a "
+                        f"{library_type.value} Library"
+                    )
+                    raise UserError(description)
+
+                if scanner.is_deprecated():
+                    description = (
+                        f"WARNING: Selected Deprecated Scanner: "
+                        f"{scanner.get_label()}"
                     )
                     PlexUtilLogger.get_logger().warning(description)
-
                 return scanner
 
-        description = f"Scanner not supported: {candidate}"
-        raise ValueError(description)
+        description = f"Scanner not found: {candidate}"
+        raise UserError(description)
+
+    @staticmethod
+    def get_default(library_type: LibraryType) -> Scanner:
+        """
+        Gets the default Scanner for a supplied LibraryType
+
+        Args:
+            library_type (LibraryType): To determine default Scanner
+
+        Returns:
+            Scanner: The Default Scanner for the supplied LibraryType
+
+        Raises:
+            LibraryUnsupportedError: If LibraryType not Movie/TV/Music
+        """
+        if library_type is LibraryType.MOVIE:
+            return Scanner.MOVIE
+        elif library_type is LibraryType.TV:
+            return Scanner.TV
+        elif library_type is LibraryType.MUSIC:
+            return Scanner.MUSIC
+        else:
+            op_type = "Scanner Get Default"
+            raise LibraryUnsupportedError(op_type, library_type)
+
+    def get_value(self) -> str:
+        """
+        Value is the canonical name of the Scanner in the Plex Server
+
+        Returns:
+            str: This Scanner canonical name
+        """
+        return self.value
+
+    def get_label(self) -> str:
+        """
+        Label is the Display Name of the Scanner in the GUI
+
+        Returns:
+            str: The Scanner Display Name
+        """
+        return self.value
