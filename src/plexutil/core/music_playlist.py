6e1ad3ea66717d90dd8e7d980437cfa2d73d5704
Carlos Florez <carlos@florez.co.uk>
2026-01-23 21:53:56 +0000

    fix(MusicPlaylist): Referencing module instead of class property

--- src/plexutil/core/music_playlist.py
+++ src/plexutil/core/music_playlist.py
@@ -5,7 +5,6 @@ from typing import TYPE_CHECKING, cast
 
 from plexutil.core.prompt import Prompt
 from plexutil.dto.dropdown_item_dto import DropdownItemDTO
-from plexutil.enums import user_request
 from plexutil.service.music_playlist_service import MusicPlaylistService
 from plexutil.service.song_music_playlist_composite_service import (
     SongMusicPlaylistCompositeService,
@@ -74,8 +73,8 @@ class MusicPlaylist(Library):
     def display(self, expect_input: bool = False) -> None:
         super().display(expect_input=True)
         if (
-            user_request is UserRequest.DOWNLOAD
-            or user_request is UserRequest.UPLOAD
+            self.user_request is UserRequest.DOWNLOAD
+            or self.user_request is UserRequest.UPLOAD
         ):
             return
         dropdown = []
